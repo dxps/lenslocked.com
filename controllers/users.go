d3a369429115898b3278f46b974389b56ccf0172
Max Li <maxlivinci@gmail.com>
2018-12-11 08:37:03 +0800

    creating the user model

--- controllers/users.go
+++ controllers/users.go
@@ -4,22 +4,25 @@ import (
 	"fmt"
 	"net/http"
 
+	"lenslocked.com/models"
 	"lenslocked.com/views"
 )
 
+func NewUsers(us *models.UserService) *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "users/new"),
+		us:      us,
+	}
+}
+
 type Users struct {
 	NewView *views.View
+	us      *models.UserService
 }
 
-type SignupForm struct {
-	Email    string `schema:"email"`
-	Password string `schema:"password"`
-}
-
-func NewUsers() *Users {
-	return &Users{NewView: views.NewView("bootstrap", "users/new")}
-}
-
+// New is used to render the form where a user can
+// create a new user account.
+//
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
@@ -27,12 +30,28 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type SignupForm struct {
+	Name     string `schema:"name"`
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Create is used to process the signup form when a user
+// tries to create a new user account.
+//
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, "Email is", form.Email)
-	fmt.Fprintln(w, "Password is", form.Password)
+	user := models.User{
+		Name:  form.Name,
+		Email: form.Email,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, "User is", user)
 }
--- main.go
+++ main.go
@@ -1,38 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"lenslocked.com/controllers"
-	"lenslocked.com/views"
+	"lenslocked.com/models"
+
+	"github.com/gorilla/mux"
 )
 
-var (
-	homeView    *views.View
-	contactView *views.View
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "azhen"
+	password = ""
+	dbname   = "lenslocked_dev"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
+func main() {
+	// Create a DB connection string and then use it to
+	// create our model services.
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-// A helper function that panics on any error func must(err error) {
-func must(err error) {
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-}
+	defer us.Close()
+	us.AutoMigrate()
 
-func main() {
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
